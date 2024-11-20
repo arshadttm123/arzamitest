15ba5bb86b826709368aae9d0bb0d1bd95056983
arshadttm123 <arshadttm.uk@gmail.com>
2024-11-20 15:41:04 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example interaction for adding items to the cart
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Item added to the cart!');
+    });
+});
